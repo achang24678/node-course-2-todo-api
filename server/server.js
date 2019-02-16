0f265bd1523607f81d8c444c424dca092065eebc
Allen Chang <achang24678@gmail.com>
2019-02-15 18:27:37 -0800

    add test cases for Get /todos/:id

--- server/server.js
+++ server/server.js
@@ -46,7 +46,7 @@ app.get('/todos/:id', (req, res) => {
   }
   Todo.findById(id).then((todo) => {    //findById
     if(!todo) {                         // if no todo - send back 404 with empty body
-      return res.status(404).sned();
+      return res.status(404).send();
     }
     res.send({todo});                 // if todo - send it back with {object} body
   }).catch((e) => {                   //error
